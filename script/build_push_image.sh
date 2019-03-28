948327188a194e166ccc83c6db048f4ee3f7b3e7
hzlvzimin <hzlvzimin@corp.netease.com>
2019-03-28 16:32:15 +0800

    Update build_push_image.sh

--- script/build_push_image.sh
+++ script/build_push_image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # build docker image
-docker build -t knative-java-demo ./
+docker build -t knative-java-demo ../
 docker tag knative-java-demo g4seek/knative-java-demo
 # login before push
 # docker login
