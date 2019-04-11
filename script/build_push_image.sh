d929b000d3fcc8497ee91bdc410dc1be15042eb1
hzlvzimin <hzlvzimin@corp.netease.com>
2019-04-11 18:03:28 +0800

    change:删除docker构建脚本

--- script/build_push_image.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# build docker image
-docker build -t knative-java-demo ../
-docker tag knative-java-demo g4seek/knative-java-demo
-# login before push
-# docker login
-docker push g4seek/knative-java-demo
